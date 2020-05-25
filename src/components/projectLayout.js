3d4a1ac9bebb1917f62a27c322f2491dedeef385
acodedoer <misbahu.s.z@gmail.com>
2020-05-25 12:24:31 +0100

    modularising materia ui navigation

--- src/components/projectLayout.js
+++ src/components/projectLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout'
-import SEO from '../components/SEO'
+import SEO from '../components/seo'
 
 export default function ProjectLayput({siteTitle, children}) {
   return (
